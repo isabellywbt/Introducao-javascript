cb1621218524a8be6020846c9fd15e542255729c
isabellywbt <isabelly.ferreira.assis@escola.pr.gov.br>
2025-11-12 12:01:08 -0300

    adicionar paciente

--- principal.js
+++ principal.js
@@ -41,4 +41,40 @@ for (var i=0; i < pacientes.length; i++) {
         tdImc.textContent = imc.toFixed(2);
     }
 
+
 }
+
+
+
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+
+
+botaoAdicionar.addEventListener("click", function(){
+    
+    var fora = document.querySelector("#fora-adiciona");
+    var nome = form.nome.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = fora.gordura.value;
+
+    var pacienteTr = document.createElement("tr");
+    var nomeTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    var imcTD = document.create.Eelement("td");
+
+    nomeTD.textContent = nome;
+    pesoTd.textContext = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+
+    var tabela = document.querySelector("#tabela-paciente");
+
+    tabela.appendChild(pacienteTr);
+});
