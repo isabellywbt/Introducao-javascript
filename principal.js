300abd9fff65f015a400c1f2957f06646e0d4f7f
isabellywbt <isabelly.ferreira.assis@escola.pr.gov.br>
2025-11-17 09:29:07 -0300

    Inserir novo paciente

--- principal.js
+++ principal.js
@@ -41,31 +41,27 @@ for (var i=0; i < pacientes.length; i++) {
         tdImc.textContent = imc.toFixed(2);
     }
 
-
 }
-
-
 
 var botaoAdicionar = document.querySelector("#adicionar-paciente");
 
-
 botaoAdicionar.addEventListener("click", function(){
     
-    var fora = document.querySelector("#fora-adiciona");
+    var form = document.querySelector("#form-adiciona");
     var nome = form.nome.value;
     var peso = form.peso.value;
     var altura = form.altura.value;
-    var gordura = fora.gordura.value;
+    var gordura = form.gordura.value;
 
     var pacienteTr = document.createElement("tr");
     var nomeTd = document.createElement("td");
     var pesoTd = document.createElement("td");
     var alturaTd = document.createElement("td");
     var gorduraTd = document.createElement("td");
-    var imcTD = document.create.Eelement("td");
+    var imcTd = document.createElement("td");
 
-    nomeTD.textContent = nome;
-    pesoTd.textContext = peso;
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
     alturaTd.textContent = altura;
     gorduraTd.textContent = gordura;
 
@@ -74,7 +70,8 @@ botaoAdicionar.addEventListener("click", function(){
     pacienteTr.appendChild(alturaTd);
     pacienteTr.appendChild(gorduraTd);
 
-    var tabela = document.querySelector("#tabela-paciente");
+    var tabela = document.querySelector("#tabela-pacientes");
 
     tabela.appendChild(pacienteTr);
+
 });
